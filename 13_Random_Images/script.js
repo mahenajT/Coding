5693a396146904f8ccc7ff44a4fdc65cc741bcb9
mahenajT <mahenaj200815@gmail.com>
2024-10-25 08:27:00 +0600

    13_Random_Images

---
+++ 13_Random_Images/script.js
@@ -0,0 +1,15 @@
+const imageContainer = document.querySelector(".image-container");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  imageNum = 20;
+  addNewImages();
+});
+function addNewImages() {
+  for (let index = 0; index < imageNum; index++) {
+    const newImage = document.createElement("img");
+    newImage.src = `https://picsum.photos/300?random=${Math.floor(
+      Math.random() * 2000
+    )}`;
+    imageContainer.appendChild(newImage);
+  }
+}
