e5dc7aea46688f7d5541c7b25ae355291c502e3f
mahenajT <mahenaj200815@gmail.com>
2024-10-26 17:25:04 +0600

    13_Button Effect

--- 13_Random_Images/script.js
+++ 13_Random_Images/script.js
@@ -13,3 +13,11 @@ function addNewImages() {
     imageContainer.appendChild(newImage);
   }
 }
+
+// Button Effect
+btn.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btn.offsetLeft;
+  const y = event.pageY - btn.offsetTop;
+  btn.style.setProperty("--xPosition", x + "px");
+  btn.style.setProperty("--yPosition", y + "px");
+});
