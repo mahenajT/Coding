598cc90921da2a4d704473c90c881dd20c014a40
mahenajT <mahenaj200815@gmail.com>
2024-11-01 06:31:26 +0600

    23_Image_Slider

--- 23_Image_Slider/app.js
+++ 23_Image_Slider/app.js
@@ -1,1 +1,11 @@
-// 
+let next = document.querySelector(".next");
+let preview = document.querySelector(".prev");
+
+next.addEventListener("click", () => {
+  let imageItems = document.querySelectorAll(".item");
+  document.querySelector(".slide").appendChild(imageItems[0]);
+});
+preview.addEventListener("click", () => {
+  let imageItems = document.querySelectorAll(".item");
+  document.querySelector(".slide").prepend(imageItems[imageItems.length - 1]);
+});
